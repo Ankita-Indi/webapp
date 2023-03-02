d0e6c9b3bfb2447044bf8bb267abc6fd9a6f0920
ankitaindi <indi.a@northeastern.edu>
2023-03-01 22:04:22 -0500

    API update

--- setup.sh
+++ setup.sh
@@ -1,54 +1,28 @@
 #!/bin/bash
-
-# sudo cp -r ./uploads/ /home/ec2/
 echo 'Updating Packages'
 sudo yum update -y
-
+sleep 10
 echo 'cleaning cache'
 sudo yum clean all
 sudo yum makecache
 echo 'installing Open jdk 8'
 cd ~
 sudo yum install wget -y
+echo "sleeping....."
 sleep 10
 sudo yum install java-1.8.0-openjdk -y
+echo "sleeping....."
 sleep 10
 java -version
 echo 'installed java successfully.'
 
-
 sudo wget https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
 sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
 sudo rpm -Uvh mysql80-community-release-el7-3.noarch.rpm
-sudo yum install mysql-server -y
 echo "sleeping....."
 sleep 10
-# sudo systemctl start mysqld.service
-# sudo systemctl status mysqld.service
-echo "sleeping....."
-sleep 10
-# pwd = sudo grep -op 'temporary password(.*): \K(\S+)' /var/log/mysqld.log
-sudo yum -y install mysql-community-server
-sudo systemctl enable --now mysqld
-systemctl status mysqld
-passwords=$(sudo grep 'temporary password' /var/log/mysqld.log | awk {'print $13'})
-sudo mysql --connect-expired-password -u root -p$passwords -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Pratappk@890'"
-# sudo mysql --connect-expired-password -u root -p$pwd "Alter USER 'root'@'localhost' IDENTIFIED BY 'Pratappk@890'"
-# pwd=`sudo grep 'temporary password' /var/log/mysqld.log | rev | cut -d':' -f 1 | rev | xargs`
-# sudo mysql -uroot -p$pwd --connect-expired-password -e "Alter user 'root'@'localhost' IDENTIFIED 
- #mysql_native_password BY 'Pratappk@890'"
- mysql -uroot -pPratappk@890  -e "CREATE DATABASE IF NOT EXISTS cloud"
+sudo yum install -y mysql-community-client
 
-pwd
-# sudo mv health-check-api-0.0.1-SNAPSHOT.jar /tmp/
 sudo chmod 544 /tmp/health-check-api.service
 sudo mv /tmp/health-check-api.service /etc/systemd/system/health-check-api.service
-# sudo chmod 544 health-check-api-0.0.1-SNAPSHOT.jar
-# sudo mkdir webapp
 
-# sudo mv health-check-api-0.0.1-SNAPSHOT.jar /usr/bin/
-sudo systemctl daemon-reload
-sudo systemctl enable health-check-api.service
-sudo systemctl start health-check-api.service
-sleep 30
-sudo systemctl status health-check-api.service
--- variable.tf
+++
@@ -1,6 +0,0 @@
-aws_region="us-east-1"
-source_ami="ami-0dfcb1ef8550277af"
-ssh_username="ec2-user"
-profile="dev"
-aws_demouser="719338688171"
-aws_devuser="966364109421"
